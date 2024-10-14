7fd0536b66ba314ba01a404c926148841f3ee1ec
root <root@vulcain.lstdr.netcup>
2024-10-14 21:02:59 +0200

    Mon Oct 14 09:02:59 PM CEST 2024

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-wget https://thespacedevs.com/iframe/iac-2024.html -o iac-2024.html
+wget -o iac-2024.html https://thespacedevs.com/iframe/iac-2024.html
 git add -A
 git commit -m "$(date)"
 git push
